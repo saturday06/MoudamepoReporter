051ec03c126168e85f0bd1f4691b776607bfadb3
Isamu Mogi <isamu@leafytree.jp>
2015-08-01 14:51:53 +0900

    oh...

--- app/src/androidTest/scala/jp/pxv/android/dailyreportchecker/ApplicationTest.scala
+++ app/src/androidTest/scala/jp/pxv/android/dailyreportchecker/ApplicationTest.scala
@@ -2,6 +2,10 @@ package jp.pxv.android.dailyreportchecker
 
 import android.app.Application
 import android.test.ApplicationTestCase
+import org.junit.Assert
 
 class ApplicationTest extends ApplicationTestCase[Application](classOf[Application]) {
+  def testFoo(): Unit = {
+    Assert.assertTrue(true)
+  }
 }
--- app/src/androidTest/scala/jp/pxv/android/dailyreportchecker/MultiDexAndroidJUnitRunner.scala
+++ app/src/androidTest/scala/jp/pxv/android/dailyreportchecker/MultiDexAndroidJUnitRunner.scala
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.multidex.MultiDex
 import android.support.test.runner.AndroidJUnitRunner
 
-class TestRunner extends AndroidJUnitRunner {
+class MultiDexAndroidJUnitRunner extends AndroidJUnitRunner {
   override def onCreate(arguments: Bundle): Unit = {
     MultiDex.install(getTargetContext)
     super.onCreate(arguments)
